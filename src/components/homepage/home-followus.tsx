ba814cb53d60f0a5d36526b6be2c7e516d76e005
Setthawut Pimthong <oop_best@yahoo.com>
2025-01-10 10:35:19 +0700

    update logo

--- src/components/homepage/home-followus.tsx
+++ src/components/homepage/home-followus.tsx
@@ -100,11 +100,11 @@ const HomeFollowUs = () => {
               <p>ไปยังเว็บไซต์</p>
               <div className="flex pt-3">
                 <Image
-                  width={106}
+                  width={76}
                   height={21}
-                  src="/images/switchflex-logo.svg"
-                  alt="Switchflex"
-                  className="object-contain"
+                  src="/images/logo-alectric.svg"
+                  alt="Alectric"
+                  className="object-contain me-3"
                 />
                 <Link
                   href="#"
